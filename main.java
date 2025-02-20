a07f4ae84d2def88fa635d7d111a3c2be4c399f9
sagar kumar <117718554+sagarkumar962@users.noreply.github.com>
2025-02-20 09:57:16 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main{
+    public static void main(String[] args) {
+        
+
+        Scanner sc = new Scanner(System.in);
+        int  side = sc.nextInt();
+        System.out.println("area of sqaure: "+side*side);
+       
+        
+    }
+}
+
